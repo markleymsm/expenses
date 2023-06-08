585fbb5482ceb14e3509b3a7117be9d6594ee2b7
Markley Mendes <markley.mendes@gmail.com>
2023-06-08 10:34:31 -0300

    Adicionando ajustes para tela incial

--- lib/main.dart
+++ lib/main.dart
@@ -19,10 +19,22 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Despesas pessoais')),
-      body: const Center(
-        child: Text('Versão inicial'),
-      ),
-    );
+        appBar: AppBar(title: const Text('Despesas pessoais')),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              child: Card(
+                color: Colors.blue,
+                child: Text('Grafico'),
+                elevation: 5,
+              ),
+            ),
+            Card(
+              child: Text('Lista de transções'),
+            )
+          ],
+        ));
   }
 }
