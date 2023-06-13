6a5dfc7cdcfa1faa8fe909f472a10d160e6171f5
Markley Mendes <markley.mendes@gmail.com>
2023-06-13 13:10:41 -0300

    add clone modal

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,8 @@ class _MyHomePageState extends State<MyHomePage> {
     setState(() {
       _transactions.add(newTransaction);
     });
+
+    Navigator.of(context).pop();
   }
 
   _openTransactionFormModal(BuildContext context) {
