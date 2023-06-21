d8833ca2b6eef60b19a383954ead9df920f0fb7f
Markley Mendes <markley.mendes@gmail.com>
2023-06-20 21:44:40 -0300

    adicionado switch para trabalhar em modo retrato

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [];
+  bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
     return _transactions.where((element) {
@@ -114,14 +115,30 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            SizedBox(
-              height: availebleHeigth * 0.30,
-              child: Chart(_recentTransactions),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text('Exibir Gráfico'),
+                Switch(
+                  value: _showChart,
+                  onChanged: (value) {
+                    setState(() {
+                      _showChart = value;
+                    });
+                  },
+                ),
+              ],
             ),
-            SizedBox(
-              height: availebleHeigth * 0.70,
-              child: TransactionList(_transactions, _removeTransaction),
-            )
+            if (_showChart)
+              SizedBox(
+                height: availebleHeigth * 0.30,
+                child: Chart(_recentTransactions),
+              ),
+            if (!_showChart)
+              SizedBox(
+                height: availebleHeigth * 0.70,
+                child: TransactionList(_transactions, _removeTransaction),
+              ),
           ],
         ),
       ),
