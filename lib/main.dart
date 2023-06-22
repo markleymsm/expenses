2a815dc78b2dcbc36f04c26d8b19fa4db9a395e9
Markley Mendes <markley.mendes@gmail.com>
2023-06-22 20:50:08 -0300

    Ajuste para o app trabalhar com ios

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,41 @@
-import 'dart:io';
 import 'dart:math';
+import 'dart:io';
+
 import 'package:expenses/components/transaction_form.dart';
-import 'package:expenses/components/transaction_list.dart';
-import 'package:expenses/components/chart.dart';
-
-import 'package:expenses/models/transaction.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
-main() => runApp(const ExpensesApp());
+import 'components/transaction_list.dart';
+import 'components/chart.dart';
+import 'models/transaction.dart';
+
+main() => runApp(ExpensesApp());
 
 class ExpensesApp extends StatelessWidget {
-  const ExpensesApp({Key? key}) : super(key: key);
+  ExpensesApp({Key? key}) : super(key: key);
+  final ThemeData tema = ThemeData();
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: const MyHomePage(),
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.purple,
-        ).copyWith(
+      theme: tema.copyWith(
+        colorScheme: tema.colorScheme.copyWith(
+          primary: Colors.purple,
           secondary: Colors.amber,
         ),
-        fontFamily: 'Quicksand',
-        textTheme: ThemeData.light().textTheme.copyWith(
-            titleLarge: const TextStyle(
-              fontFamily: 'OpenSans',
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
-            ),
-            labelLarge: const TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.bold,
-            )),
+        textTheme: tema.textTheme.copyWith(
+          titleLarge: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
+          labelLarge: const TextStyle(
+            color: Colors.white,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         appBarTheme: const AppBarTheme(
           titleTextStyle: TextStyle(
             fontFamily: 'OpenSans',
@@ -58,8 +60,8 @@ class _MyHomePageState extends State<MyHomePage> {
   bool _showChart = false;
 
   List<Transaction> get _recentTransactions {
-    return _transactions.where((element) {
-      return element.date.isAfter(DateTime.now().subtract(
+    return _transactions.where((tr) {
+      return tr.date.isAfter(DateTime.now().subtract(
         const Duration(days: 7),
       ));
     }).toList();
@@ -82,71 +84,83 @@ class _MyHomePageState extends State<MyHomePage> {
 
   _removeTransaction(String id) {
     setState(() {
-      _transactions.removeWhere((element) => element.id == id);
+      _transactions.removeWhere((tr) => tr.id == id);
     });
   }
 
   _openTransactionFormModal(BuildContext context) {
     showModalBottomSheet(
-        context: context,
-        builder: (_) {
-          return TransactionForm(_addTransaction);
-        });
+      context: context,
+      builder: (_) {
+        return TransactionForm(_addTransaction);
+      },
+    );
   }
 
-  Widget _getIconButton;
+  Widget _getIconButton(IconData icon, Function() fn) {
+    return Platform.isIOS
+        ? GestureDetector(onTap: fn, child: Icon(icon))
+        : IconButton(icon: Icon(icon), onPressed: fn);
+  }
 
   @override
   Widget build(BuildContext context) {
     final mediaQuery = MediaQuery.of(context);
-    bool isLandScape = mediaQuery.orientation == Orientation.landscape;
+    bool isLandscape = mediaQuery.orientation == Orientation.landscape;
 
     final actions = [
-      if (isLandScape)
-        IconButton(
-          onPressed: () {
+      if (isLandscape)
+        _getIconButton(
+          _showChart ? Icons.list : Icons.show_chart,
+          () {
             setState(() {
               _showChart = !_showChart;
             });
           },
-          icon: Icon(_showChart ? Icons.list : Icons.pie_chart),
         ),
-      IconButton(
-        onPressed: () => _openTransactionFormModal(context),
-        icon: const Icon(Icons.add),
+      _getIconButton(
+        Platform.isIOS ? CupertinoIcons.add : Icons.add,
+        () => _openTransactionFormModal(context),
       ),
     ];
 
-    final PreferredSizeWidget appBar = Platform.isIOS
-        ? CupertinoNavigationBar(
-            middle: Text('Despesas Pessoais'),
-            trailing: Row(
-              children: actions,
-            ),
-          )
-        : AppBar(
-            title: const Text(
-              'Despesas pessoais',
-            ),
-            actions: actions,
-          );
+    final PreferredSizeWidget appBar = AppBar(
+      title: const Text('Despesas Pessoais'),
+      actions: actions,
+    );
 
-    final availebleHeigth = mediaQuery.size.height -
+    final availableHeight = mediaQuery.size.height -
         appBar.preferredSize.height -
         mediaQuery.padding.top;
 
-    final boydPage = SingleChildScrollView(
+    final bodyPage = SingleChildScrollView(
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         children: [
-          if (_showChart || isLandScape)
+          // if (isLandscape)
+          //   Row(
+          //     mainAxisAlignment: MainAxisAlignment.center,
+          //     children: [
+          //       const Text('Exibir Gráfico'),
+          //       Switch.adaptive(
+          //         activeColor: Theme.of(context).colorScheme.secondary,
+          //         value: _showChart,
+          //         onChanged: (value) {
+          //           setState(() {
+          //             _showChart = value;
+          //           });
+          //         },
+          //       ),
+          //     ],
+          //   ),
+          if (_showChart || !isLandscape)
             SizedBox(
-              height: availebleHeigth * (isLandScape ? 0.8 : 0.30),
+              height: availableHeight * (isLandscape ? 0.8 : 0.3),
               child: Chart(_recentTransactions),
             ),
-          if (!_showChart || !isLandScape)
+          if (!_showChart || !isLandscape)
             SizedBox(
-              height: availebleHeigth * (isLandScape ? 1 : 0.8),
+              height: availableHeight * (isLandscape ? 1 : 0.7),
               child: TransactionList(_transactions, _removeTransaction),
             ),
         ],
@@ -155,17 +169,23 @@ class _MyHomePageState extends State<MyHomePage> {
 
     return Platform.isIOS
         ? CupertinoPageScaffold(
-            navigationBar: appBar,
-            child: boydPage,
+            navigationBar: CupertinoNavigationBar(
+              middle: const Text('Despesas Pessoais'),
+              trailing: Row(
+                mainAxisSize: MainAxisSize.min,
+                children: actions,
+              ),
+            ),
+            child: bodyPage,
           )
         : Scaffold(
             appBar: appBar,
-            body: boydPage,
+            body: bodyPage,
             floatingActionButton: Platform.isIOS
                 ? Container()
                 : FloatingActionButton(
-                    onPressed: () => _openTransactionFormModal(context),
                     child: const Icon(Icons.add),
+                    onPressed: () => _openTransactionFormModal(context),
                   ),
             floatingActionButtonLocation:
                 FloatingActionButtonLocation.centerFloat,
