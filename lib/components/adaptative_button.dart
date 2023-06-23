5678161111367cb3f85931716499eab7d24144d7
Markley Mendes <markley.mendes@gmail.com>
2023-06-22 21:09:58 -0300

    Criando adaptacoes para botoes trabalhando em IOS e Android

---
+++ lib/components/adaptative_button.dart
@@ -0,0 +1,35 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:io';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class AdaptativeButton extends StatelessWidget {
+  final String label;
+  final Function() onPressed;
+
+  const AdaptativeButton(
+    this.label,
+    this.onPressed, {
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Platform.isIOS
+        ? CupertinoButton(
+            onPressed: onPressed,
+            color: Theme.of(context).colorScheme.primary,
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: Text(label),
+          )
+        : ElevatedButton(
+            onPressed: onPressed,
+            child: Text(
+              label,
+              style: TextStyle(
+                  color: Theme.of(context).textTheme.labelLarge?.color),
+            ),
+          );
+  }
+}
